8255ed06d9f6c68feb065e6161d2a0d7a51e3266
IcePic <icepic1984@gmx.de>
2019-03-11 00:33:57 +0100

    Add some tests

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,5 +3,13 @@
 
 int main()
 {
-    std::cout << "Yeah" << std::endl;
+    auto bla = eclpp::convert<int>::to_ecl(10);
+    std::cout << eclpp::convert<int>::to_cpp(bla) << std::endl;
+
+    // auto blup = eclpp::convert<double>::to_ecl(1.0);
+    // std::cout << eclpp::convert<double>::to_cpp(blup) << std::endl;
+
+    cl_object b = ecl_make_long_float(10.0);
+    double d = ecl_to_long_double(b);
+    std::cout << d << std::endl;
 }
