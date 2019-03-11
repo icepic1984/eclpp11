15dd42f19bdc991afef4e02f516288bb9bac1aac
Christoph Buck <Christoph.Buck@smartoptics.de>
2019-03-11 11:32:35 +0100

    Use macros to generate conversion functions

--- eclpp/eclpp.hpp
+++ eclpp/eclpp.hpp
@@ -7,80 +7,53 @@ namespace eclpp
 template <typename T, typename Enable = void>
 struct convert;
 
-template <>
-struct convert<int>
+#define ECL_DECLTYPE_RETURN(...)                                               \
+    decltype(__VA_ARGS__)                                                      \
+    {                                                                          \
+        return __VA_ARGS__;                                                    \
+    }
+
+double ecl_to_double_float(cl_object v)
 {
-    static int to_cpp(cl_object v)
-    {
-        return fix(v);
-    }
+    return ecl_double_float(v);
+}
 
-    static cl_object to_ecl(int v)
-    {
-        return ecl_make_integer(v);
-    }
-};
-
-template <>
-struct convert<std::uint8_t>
+double ecl_to_single_float(cl_object v)
 {
-    static std::uint8_t to_cpp(cl_object v)
-    {
-        return ecl_to_uint8_t(v);
-    }
+    return ecl_single_float(v);
+}
 
-    static cl_object to_ecl(std::uint8_t v)
-    {
-        return ecl_make_uint8_t(v);
-    }
-};
+#define ECL_DECLARE_NUMERIC_TYPE(cpp_name__, ecl_name__)                       \
+    template <>                                                                \
+    struct convert<cpp_name__>                                                 \
+    {                                                                          \
+        static cpp_name__ to_cpp(cl_object v)                                  \
+        {                                                                      \
+            return ecl_to_##ecl_name__(v);                                     \
+        }                                                                      \
+        static cl_object to_ecl(cpp_name__ v)                                  \
+        {                                                                      \
+            return ecl_make_##ecl_name__(v);                                   \
+        }                                                                      \
+    };
 
-template <>
-struct convert<std::int8_t>
-{
-    static std::int8_t to_cpp(cl_object v)
-    {
-        return ecl_to_int8_t(v);
-    }
+ECL_DECLARE_NUMERIC_TYPE(std::int8_t, int8_t);
+ECL_DECLARE_NUMERIC_TYPE(std::uint8_t, uint8_t);
+ECL_DECLARE_NUMERIC_TYPE(std::int16_t, int16_t);
+ECL_DECLARE_NUMERIC_TYPE(std::uint16_t, uint16_t);
+ECL_DECLARE_NUMERIC_TYPE(std::int32_t, int32_t);
+ECL_DECLARE_NUMERIC_TYPE(std::uint32_t, uint32_t);
+ECL_DECLARE_NUMERIC_TYPE(std::int64_t, int64_t);
+ECL_DECLARE_NUMERIC_TYPE(std::uint64_t, uint64_t);
+ECL_DECLARE_NUMERIC_TYPE(float, single_float);
+ECL_DECLARE_NUMERIC_TYPE(double, double_float);
 
-    static cl_object to_ecl(std::int8_t v)
-    {
-        return ecl_make_int8_t(v);
-    }
-};
+template <typename T>
+auto to_ecl(T&& v) -> ECL_DECLTYPE_RETURN(
+    convert<std::decay_t<T>>::to_ecl(std::forward<T>(v)));
 
-template <>
-struct convert<double>
-{
-    static double to_cpp(cl_object v)
-    {
-        return ecl_double_float(v);
-    }
+template <typename T>
+auto to_cpp(cl_object v)
+    -> ECL_DECLTYPE_RETURN(convert<std::decay_t<T>>::to_cpp(v));
 
-    static cl_object to_ecl(double v)
-    {
-        return ecl_make_double_float(v);
-    }
-};
-
-template <>
-struct convert<float>
-{
-    static float to_cpp(cl_object v)
-    {
-        return ecl_single_float(v);
-    }
-    static cl_object to_ecl(float v)
-    {
-        return ecl_make_single_float(v);
-    }
-};
 } // namespace eclpp
-
-// template <typename T>
-// auto to_scm(T&& v) -> ECLPP_DECLTYPE_RETURN(
-//     convert<std::decay_t<T>>::to_scm(std::forward<T>(v)));
-
-// template <typename T>
-// auto to_cpp(SCM v)
-//     -> ECLPP_DECLTYPE_RETURN(convert<std::decay_t<T>>::to_cpp(v));
