76a6b951c5c37830c7158421fee707237af87418
Christoph Buck <Christoph.Buck@smartoptics.de>
2019-03-12 16:09:18 +0100

    Add concrete using example

--- examples/module.cpp
+++ examples/module.cpp
@@ -1,9 +1,12 @@
 #include <eclpp.hpp>
 #include <iostream>
 
-struct Image
+class Image
 {
+public:
     Image()
+        : width(0)
+        , height(0)
     {
         std::cout << "Construct" << std::endl;
     }
@@ -12,21 +15,49 @@ struct Image
     {
         std::cout << "Destruct" << std::endl;
     }
+
+    Image(int w, int h)
+        : width(w)
+        , height(h)
+    {
+        std::cout << "Construct: " << width << " " << height << std::endl;
+    }
+
+    int getWidth()
+    {
+        return width;
+    }
+
+    int getHeight()
+    {
+        return height;
+    }
+
+    void setWidth(int w)
+    {
+        width = w;
+    }
+
+    void setHeight(int h)
+    {
+        height = h;
+    }
+
+private:
     int width;
     int height;
 };
 
-cl_object make_image()
-{
-    return eclpp::make_foreign<Image>();
-}
-
+// https://stackoverflow.com/questions/44494613/embeddable-common-lisp-ecl-load-shared-library-with-extension-functionsxo
 extern "C" {
 void init_lib(void)
 {
-    // https://stackoverflow.com/questions/44494613/embeddable-common-lisp-ecl-load-shared-library-with-extension-functionsxo
     std::cout << "Initialize library" << std::endl;
-    ecl_def_c_function(ecl_read_from_cstring("make-image"),
-        reinterpret_cast<cl_objectfn_fixed>(make_image), 0);
+    eclpp::type<Image>("image")
+        .constructor()
+        .define("get-width", &Image::getWidth)
+        .define("set-width", &Image::setWidth)
+        .define("get-height", &Image::getHeight)
+        .define("set-height", &Image::setHeight);
 }
 }
