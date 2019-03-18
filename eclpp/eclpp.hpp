d68b102d8d6db172163d24643be9b42ced90ad04
Christoph Buck <Christoph.Buck@smartoptics.de>
2019-03-18 09:53:24 +0100

    Add group definer

--- eclpp/eclpp.hpp
+++ eclpp/eclpp.hpp
@@ -383,7 +383,9 @@ struct move_sequence
 };
 // See
 // https://stackoverflow.com/questions/8610571/what-is-rvalue-reference-for-this
-// why ref qulifer on member functions are needed.
+// why ref qulifer on member functions are needed.  For ecl we
+// actually don't need the move_sequence, because foreign types are
+// not registered during destruction.
 template <typename Tag, typename T, int Seq = 0>
 struct type_definer : move_sequence
 {
@@ -473,6 +475,85 @@ template <typename T, typename Tag = T>
 type_definer<Tag, T> type(std::string type_name)
 {
     return {type_name};
+}
+
+template <typename Tag, int Seq = 0>
+struct definer
+{
+    using this_t = definer;
+    using next_t = definer<Tag, Seq + 1>;
+
+    definer() = default;
+    definer(definer&&) = default;
+
+    template <int Seq2, typename Enable = std::enable_if_t<Seq2 + 1 == Seq>>
+    definer(definer<Tag, Seq2>)
+    {
+    }
+
+    /**
+     * Defines a Scheme procedure `(name ...)` that returns the result
+     * of invoking `fn(...)`.
+     */
+    template <typename Fn>
+    next_t define(std::string name, Fn fn) &&
+    {
+        define_function(name, fn);
+        return {std::move(*this)};
+    }
+};
+
+template <typename Tag, int Seq = 0>
+struct group_definer
+{
+    using this_t = group_definer;
+    using next_t = group_definer<Tag, Seq + 1>;
+
+    std::string group_name_ = {};
+
+    group_definer(std::string name)
+        : group_name_{std::move(name)}
+    {
+    }
+
+    group_definer(group_definer&&) = default;
+
+    template <int Seq2, typename Enable = std::enable_if_t<Seq2 + 1 == Seq>>
+    group_definer(group_definer<Tag, Seq2> other)
+        : group_name_{std::move(other.group_name_)}
+    {
+    }
+
+    /**
+     * Defines a Scheme procedure `([group-name]-name ...)` that returns
+     * the result of invoking `fn(...)`.
+     */
+    template <typename Fn>
+    next_t define(std::string name, Fn fn) &&
+    {
+        define_function(group_name_ + "-" + name, fn);
+        return {std::move(*this)};
+    }
+};
+
+/**
+ * Returns a `scm::detail::definer` that can be used to add definitions
+ * to the current module.
+ */
+template <typename Tag = void>
+definer<Tag> group()
+{
+    return {};
+}
+
+/**
+ * Returns a named `scm::detail::group_definer` that can be used to add
+ * definitions to the current module.
+ */
+template <typename Tag = void>
+group_definer<Tag> group(std::string group_name)
+{
+    return {std::move(group_name)};
 }
 
 } // namespace eclpp
