8b89bdb26ad20767ad3bb4d2e4aa5ea8a95fdb5b
icepic <icepic2009@googlemail.com>
2019-05-19 19:11:59 +0200

    Add first draft of type conversion between lisp and cpp

--- clbind/examples/type_conversion.cpp
+++ clbind/examples/type_conversion.cpp
@@ -1,5 +1,38 @@
+#include <iostream>
 #include <clbind/type_conversion.hpp>
 
 int main()
 {
+    struct A
+    {
+    };
+    struct B
+    {
+    };
+    clbind::class_registry reg;
+    reg.add(typeid(A), "A");
+    reg.add(typeid(B), "B");
+
+    std::cout << reg.name(typeid(A)) << std::endl;
+    std::cout << reg.name(typeid(B)) << std::endl;
+
+    std::cout << clbind::static_type_mapping<char>::lisp_type() << std::endl;
+    std::cout << clbind::static_type_mapping<uint8_t>::lisp_type() << std::endl;
+    std::cout << clbind::static_type_mapping<unsigned char>::lisp_type()
+              << std::endl;
+
+    std::cout << clbind::static_type_mapping<int>::lisp_type() << std::endl;
+    std::cout << clbind::static_type_mapping<int32_t>::lisp_type() << std::endl;
+    std::cout << clbind::static_type_mapping<uint32_t>::lisp_type()
+              << std::endl;
+
+    std::cout << clbind::static_type_mapping<int8_t>::lisp_type() << std::endl;
+
+    clbind::convert_to_lisp<int> bla;
+
+    // clbind::convert_to_lisp<int*> blup;
+    // int i;
+    // blup.operator()<int>(&i);
+    // std::cout << bla(10) << std::endl;
+    //    clbind::convert_to_lisp<int> bla;
 }
--- clbind/include/clbind/type_conversion.hpp
+++ clbind/include/clbind/type_conversion.hpp
@@ -1,7 +1,14 @@
 #pragma once
 
+#include <unordered_map>
+#include <string>
+#include <typeindex>
+
+namespace clbind
+{
 class class_registry
 {
+public:
     void add(std::type_index index, const std::string& name)
     {
         m_reg.insert({index, name});
@@ -9,9 +16,9 @@ class class_registry
 
     std::string name(std::type_index index)
     {
-        if (auto iter = m_reg.find(index), iter != m_reg.end())
+        if (auto iter = m_reg.find(index); iter != m_reg.end())
         {
-            return iter - second;
+            return iter->second;
         }
         else
         {
@@ -22,3 +29,214 @@ class class_registry
 private:
     std::unordered_map<std::type_index, std::string> m_reg;
 };
+
+template <typename T>
+struct unused_type
+{
+};
+
+template <typename T1, typename T2>
+struct define_if_different
+{
+    using type = T1;
+};
+
+template <typename T>
+struct define_if_different<T, T>
+{
+    using type = unused_type<T>;
+};
+
+template <typename T1, typename T2>
+using define_if_different_t = typename define_if_different<T1, T2>::type;
+
+// Mapping between c+++ and lisp types
+template <typename T>
+struct static_type_mapping;
+
+template <>
+struct static_type_mapping<char>
+{
+    using type = char;
+    static std::string lisp_type()
+    {
+        return ":char";
+    }
+};
+
+template <>
+struct static_type_mapping<define_if_different_t<unsigned char, uint8_t>>
+{
+    using type = unsigned char;
+    static std::string lisp_type()
+    {
+        return ":unsigned-char";
+    }
+};
+
+template <>
+struct static_type_mapping<define_if_different_t<int, int32_t>>
+{
+    using type = int;
+    static std::string lisp_type()
+    {
+        return ":int";
+    }
+};
+
+template <>
+struct static_type_mapping<uint8_t>
+{
+    using type = uint8_t;
+    static std::string lisp_type()
+    {
+        return ":uint8-t";
+    }
+};
+
+template <>
+struct static_type_mapping<int8_t>
+{
+    using type = int8_t;
+    static std::string lisp_type()
+    {
+        return ":int8-t";
+    }
+};
+
+template <>
+struct static_type_mapping<uint16_t>
+{
+    using type = uint16_t;
+    static std::string lisp_type()
+    {
+        return ":uint16-t";
+    }
+};
+
+template <>
+struct static_type_mapping<int16_t>
+{
+    using type = int16_t;
+    static std::string lisp_type()
+    {
+        return ":int16-t";
+    }
+};
+
+template <>
+struct static_type_mapping<uint32_t>
+{
+    using type = uint32_t;
+    static std::string lisp_type()
+    {
+        return ":uint32-t";
+    }
+};
+
+template <>
+struct static_type_mapping<int32_t>
+{
+    using type = int32_t;
+    static std::string lisp_type()
+    {
+        return ":int32-t";
+    }
+};
+
+template <>
+struct static_type_mapping<uint64_t>
+{
+    using type = uint64_t;
+    static std::string lisp_type()
+    {
+        return ":uint64-t";
+    }
+};
+
+template <>
+struct static_type_mapping<int64_t>
+{
+    using type = int64_t;
+    static std::string lisp_type()
+    {
+        return ":int64-t";
+    }
+};
+
+template <>
+struct static_type_mapping<float>
+{
+    using type = float;
+    static std::string lisp_type()
+    {
+        return ":float";
+    }
+};
+
+template <>
+struct static_type_mapping<double>
+{
+    using type = double;
+    static std::string lisp_type()
+    {
+        return ":double";
+    }
+};
+
+template <typename T>
+using static_type_mapping_t = typename static_type_mapping<T>::type;
+
+template <typename CppT, typename Enable = void>
+struct convert_to_lisp
+{
+    template <typename LispT>
+    LispT* operator()(CppT&&)
+    {
+        static_assert(sizeof(CppT) == 0,
+            "No appropriate specialization for convert_to_lisp");
+        return nullptr;
+    }
+};
+
+template <typename CppT>
+struct convert_to_lisp<CppT,
+    typename std::enable_if<std::is_fundamental<CppT>::value>::type>
+{
+    CppT operator()(CppT cpp) const
+    {
+        return cpp;
+    }
+};
+
+template <typename T>
+using convert_to_lisp_t = convert_to_lisp<T>;
+
+template <typename T>
+auto convert_to_lisp_helper(T&& cpp_val)
+    -> decltype(convert_to_lisp<T>()(std::forward<T>> (cpp_val)))
+{
+    return convert_to_lisp<T>()(std::forward<T>(cpp_val));
+}
+
+// template <typename R, typename... Args>
+// struct ReturnTypeAdapter
+// {
+//     using return_type =
+//     decltype(static_type_mapping(std::declval<R>())::type);
+
+//     return_type operator()(
+//         const void* functor, static_type_mapping_t<Args>... args)
+//     {
+//         auto std_func =
+//             reinterpret_cast<const std::function<R(Args...)>*>(functor);
+//     };
+// };
+
+// template <typename R, typename... Args>
+// struct CallFunctor
+// {
+//     using return_type
+// }
+
+} // namespace clbind
