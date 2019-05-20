8444a4f6d4039c749611720920d7e30666b0a597
Christoph Buck <Christoph.Buck@smartoptics.de>
2019-05-20 18:16:44 +0200

    Add experimental type conversions

--- clbind/examples/type_conversion.cpp
+++ clbind/examples/type_conversion.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include <clbind/type_conversion.hpp>
 
-int main()
+int funcptr(int a, int b)
 {
+    return a + b;
+}
+
+struct functor
+{
+    int operator()(int a)
+    {
+        return a;
+    }
+};
+
+struct member
+{
+    int test(int a, int b)
+    {
+        return a + b;
+    }
+};
+
+int main(int argc, char** args)
+{
+    cl_boot(argc, args);
     struct A
     {
     };
@@ -28,8 +50,46 @@ int main()
 
     std::cout << clbind::static_type_mapping<int8_t>::lisp_type() << std::endl;
 
-    clbind::convert_to_lisp<int> bla;
+    // clbind::convert_to_lisp<int> bla;
 
+    std::cout << "f1" << std::endl;
+    clbind::cpp_function f1(funcptr);
+    std::cout << "f2" << std::endl;
+    clbind::cpp_function f2(functor{});
+    std::cout << "f3" << std::endl;
+    clbind::cpp_function f3([](int a) { return a; });
+    std::cout << "f4" << std::endl;
+    int a = 10;
+    double tmp = 20.0;
+    clbind::cpp_function f4([a, tmp](int b) { return a + b + tmp; });
+
+    std::cout << "f5" << std::endl;
+    member m;
+    clbind::cpp_function f5(member::test);
+    std::cout << "Done" << std::endl;
+
+    int ecl_a = 10;
+    int& ecl_ref = ecl_a;
+
+    struct Ecl_A
+    {
+    };
+
+    Ecl_A ecl_struct;
+    clbind::to_ecl(ecl_a);
+    clbind::to_ecl(&ecl_a);
+    clbind::to_ecl(ecl_ref);
+    clbind::to_ecl(10);
+    clbind::to_ecl(ecl_struct);
+    clbind::to_ecl(&ecl_struct);
+    clbind::to_ecl(Ecl_A{});
+    clbind::to_ecl(ecl_ref);
+
+    // lisp
+    // (a 10 20)
+
+    // (cl_object:func_a cl_object:int_10 cl_object:int_20)
+    //
     // clbind::convert_to_lisp<int*> blup;
     // int i;
     // blup.operator()<int>(&i);
--- clbind/include/clbind/type_conversion.hpp
+++ clbind/include/clbind/type_conversion.hpp
@@ -3,7 +3,7 @@
 #include <unordered_map>
 #include <string>
 #include <typeindex>
-
+#include <ecl/ecl.h>
 namespace clbind
 {
 class class_registry
@@ -219,6 +219,120 @@ auto convert_to_lisp_helper(T&& cpp_val)
     return convert_to_lisp<T>()(std::forward<T>(cpp_val));
 }
 
+template <class... Ts>
+using all_of = std::conjunction<Ts...>;
+template <class... Ts>
+using any_of = std::disjunction<Ts...>;
+template <class... Ts>
+using none_of = std::negation<any_of<Ts...>>;
+
+template <class T, template <class> class... Predicates>
+using satisfies_all_of = all_of<Predicates<T>...>;
+template <class T, template <class> class... Predicates>
+using satisfies_any_of = any_of<Predicates<T>...>;
+template <class T, template <class> class... Predicates>
+using satisfies_none_of = none_of<Predicates<T>...>;
+
+/// Strip the class from a method type
+template <typename T>
+struct remove_class
+{
+};
+template <typename C, typename R, typename... A>
+struct remove_class<R (C::*)(A...)>
+{
+    typedef R type(A...);
+};
+template <typename C, typename R, typename... A>
+struct remove_class<R (C::*)(A...) const>
+{
+    typedef R type(A...);
+};
+
+template <typename F>
+struct strip_function_object
+{
+    using type = typename remove_class<decltype(&F::operator())>::type;
+};
+
+// Extracts the function signature from a function, function pointer or lambda.
+template <typename Function, typename F = std::remove_reference_t<Function>>
+using function_signature_t = std::conditional_t<std::is_function<F>::value, F,
+    typename std::conditional_t<std::is_pointer<F>::value
+                                    || std::is_member_pointer<F>::value,
+        std::remove_pointer<F>, strip_function_object<F>>::type>;
+
+/// Returns true if the type looks like a lambda: that is, isn't a function,
+/// pointer or member pointer.  Note that this can catch all sorts of other
+/// things, too; this is intended to be used in a place where passing a lambda
+/// makes sense.
+template <typename T>
+using is_lambda = satisfies_none_of<std::remove_reference_t<T>,
+    std::is_function, std::is_pointer, std::is_member_pointer>;
+
+class cpp_function
+{
+public:
+    cpp_function()
+    {
+    }
+    cpp_function(std::nullptr_t)
+    {
+        std::cout << "nullptr" << std::endl;
+    }
+
+    /// Construct a cpp_function from a vanilla function pointer
+    template <typename Return, typename... Args>
+    cpp_function(Return (*f)(Args...))
+    {
+        std::cout << "cpp_function(Return (*f)(Args...))" << std::endl;
+        initialize(f, f);
+    }
+
+    /// Construct a cpp_function from a lambda function (possibly with internal
+    /// state)
+    template <typename Func,
+        typename = std::enable_if_t<is_lambda<Func>::value>>
+    cpp_function(Func&& f)
+    {
+        std::cout << "cpp_function(Func&& f))" << std::endl;
+        initialize(std::forward<Func>(f), (function_signature_t<Func>*)nullptr);
+    }
+
+    /// Construct a cpp_function from a class method (non-const)
+    template <typename Return, typename Class, typename... Arg>
+    cpp_function(Return (Class::*f)(Arg...))
+    {
+        std::cout << "cpp_function(Return (Class::*f)(Arg...))" << std::endl;
+        initialize(
+            [f](Class* c, Arg... args) -> Return { return (c->*f)(args...); },
+            (Return(*)(Class*, Arg...)) nullptr);
+    }
+
+    /// Construct a cpp_function from a class method (const)
+    template <typename Return, typename Class, typename... Arg>
+    cpp_function(Return (Class::*f)(Arg...) const)
+    {
+        std::cout << "cpp_function(Return (Class::*f)(Arg...) const)"
+                  << std::endl;
+        initialize([f](const Class* c,
+                       Arg... args) -> Return { return (c->*f)(args...); },
+            (Return(*)(const Class*, Arg...)) nullptr);
+    }
+
+    template <typename Func, typename Return, typename... Args>
+
+    void initialize(Func&& f, Return (*)(Args...))
+    {
+        std::cout << "Initialize" << std::endl;
+        struct capture
+        {
+            std::remove_reference_t<Func> f;
+        };
+
+        std::cout << sizeof(capture) << std::endl;
+    }
+};
 // template <typename R, typename... Args>
 // struct ReturnTypeAdapter
 // {
@@ -238,5 +352,112 @@ auto convert_to_lisp_helper(T&& cpp_val)
 // {
 //     using return_type
 // }
+template <typename T>
+struct finalizer_storage
+{
+    static cl_object finalizer;
+};
 
+template <typename T>
+cl_object finalize(cl_object obj)
+{
+    T* data = static_cast<T*>(ecl_foreign_data_pointer_safe(obj));
+    delete data;
+    return ECL_T;
+}
+
+template <typename T>
+cl_object finalizer_storage<T>::finalizer = ECL_NIL;
+
+template <typename T, typename... Args>
+cl_object make_foreign(Args&&... args)
+{
+    using finalizer = finalizer_storage<T>;
+    cl_object foreign = ecl_make_foreign_data(
+        ECL_NIL, sizeof(T), new T(std::forward<Args>(args)...));
+    // if (finalizer::finalizer == ECL_NIL)
+    // {
+    //     finalizer::finalizer =
+    //         ecl_make_cfun(reinterpret_cast<cl_objectfn_fixed>(finalize<T>),
+    //             ecl_read_from_cstring("finalizer"), ECL_NIL, 1);
+    // }
+    // si_set_finalizer(foreign, finalizer::finalizer);
+    return foreign;
+}
+
+template <typename T>
+T& get_foreign(cl_object v)
+{
+    return *static_cast<T*>(ecl_foreign_data_pointer_safe(v));
+}
+
+template <typename T, typename Enable = void>
+struct convert;
+
+template <>
+struct convert<int32_t>
+{
+    static int32_t to_cpp(cl_object v)
+    {
+        std::cout << "int32_t" << std::endl;
+        return ecl_to_int32_t(v);
+    }
+    static cl_object to_ecl(int32_t v)
+    {
+        std::cout << "int32_t" << std::endl;
+        return ecl_make_int32_t(v);
+    }
+};
+
+template <typename T>
+struct convert<T*>
+{
+    static T* to_cpp(cl_object v)
+    {
+        std::cout << "static T* to_cpp(cl_object v)" << std::endl;
+        return reinterpret_cast<T*>(ecl_to_pointer(v));
+    }
+
+    static cl_object to_ecl(T* v)
+    {
+        std::cout << "static cl_object to_ecl(T* v)" << std::endl;
+        return ecl_make_pointer(v);
+    }
+};
+
+// Assume that every other type is foreign
+template <typename T>
+struct convert<T, std::enable_if_t<!std::is_fundamental<T>::value
+                                   && !std::is_enum<T>::value &&
+                                   // only value types are supported at
+                                   // the moment but the story might
+                                   // change later...
+                                   !std::is_pointer<T>::value>>
+{
+    template <typename U>
+    static cl_object to_ecl(U&& v)
+    {
+        std::cout << "cl_object to_ecl(U&& v)" << std::endl;
+        return make_foreign<T>(std::forward<U>(v));
+    }
+    static T& to_cpp(cl_object v)
+    {
+        std::cout << "T& to_cpp(cl_object v)" << std::endl;
+        return get_foreign<T>(v);
+    }
+};
+
+template <typename T>
+auto to_ecl(T&& v)
+    -> decltype(convert<std::decay_t<T>>::to_ecl(std::forward<T>(v)))
+{
+    return convert<std::decay_t<T>>::to_ecl(std::forward<T>(v));
+}
+
+// Helper function to convert from ecl to cpp
+template <typename T>
+auto to_cpp(cl_object v) -> decltype(convert<std::decay_t<T>>::to_cpp(v))
+{
+    return convert<std::decay_t<T>>::to_cpp(v);
+}
 } // namespace clbind
