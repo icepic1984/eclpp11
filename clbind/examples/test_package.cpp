a44ee14b4ecda8d19f14390856bcf54d32d8af21
Christoph <christoph.buck@smartoptics.de>
2019-08-07 18:07:47 +0200

    Add more generic type wrapper

--- clbind/examples/test_package.cpp
+++ clbind/examples/test_package.cpp
@@ -1,6 +1,8 @@
 #include <clbind/package.hpp>
+#include <clbind/function_traits.hpp>
 #include <ecl/internal.h>
 #include <type_traits>
+#include <functional>
 
 CLBIND_PACKAGE test(clbind::Package& package)
 {
@@ -73,12 +75,96 @@ void va(cl_narg narg, ...)
     ECL_STACK_FRAME_VARARGS_END(frame);
 }
 
+int ft_test(int a, int b, int c)
+{
+}
+
+struct ft_op
+{
+    int operator()(int a, double b)
+    {
+        return 0;
+    }
+};
+
+struct ft_member
+{
+    typedef int (*Operation)(int a, int b);
+
+    int test(int a, double b)
+    {
+        return 0;
+    }
+
+    Operation b;
+
+    int c;
+};
+
+template <typename F>
+using function_args_t = boost::callable_traits::args_t<F, std::tuple>;
+
+template <typename F>
+using function_traits_t = typename clbind::function_traits<F>::args;
+
+template <typename T>
+struct fill_tuple;
+
+template <typename... Args>
+struct fill_tuple<std::tuple<Args...>>
+{
+    template <typename F>
+    static constexpr cl_object wrap(F&& f, cl_object frame)
+    {
+        wrapImpl2(
+            std::forward<F>(f), std::index_sequence_for<Args...>{}, frame);
+    }
+
+private:
+    template <typename F, std::size_t... Index>
+    static constexpr cl_object wrapImpl2(
+        F&& f, std::index_sequence<Index...>, cl_object frame)
+    {
+        using return_type = typename clbind::function_traits<F>::return_type;
+
+        if constexpr (std::is_same_v<void, return_type>)
+        {
+
+            std::invoke(f, clbind::toCpp<std::tuple_element_t<Index, Tuple>>(
+                               clbind::nth_arg(frame, Index))...);
+            return ECL_NIL;
+        }
+        else
+        {
+            return clbind::toEcl<int>(std::invoke(
+                f, clbind::toCpp<std::tuple_element_t<Index, Tuple>>(
+                       clbind::nth_arg(frame, Index))...));
+        }
+    }
+
+    using Tuple = std::tuple<Args...>;
+};
+
+template <typename F>
+cl_object wrap2(F&& func, cl_object frame)
+{
+    fill_tuple<function_traits_t<F>>::wrap(std::forward<F>(func), frame);
+}
+
+template <typename F>
+int ft(F&& f)
+{
+    static_assert(std::is_same_v<std::tuple_element_t<0, function_args_t<F>>,
+        std::add_lvalue_reference_t<ft_member>>);
+
+    static_assert(std::is_same_v<std::tuple_element_t<0, function_traits_t<F>>,
+        std::add_lvalue_reference_t<ft_member>>);
+}
+
 int main(int argc, char** args)
 {
+
     cl_boot(argc, args);
-    clbind::FunctionTraits<decltype(j)> t1{};
-    clbind::FunctionTraits<decltype(g)> t2{};
-    clbind::FunctionTraits<decltype(f)> t3{};
 
     static_assert(
         std::is_same_v<clbind::FunctionTraits<decltype(j)>::ArgumentType<0>,
@@ -116,10 +202,18 @@ int main(int argc, char** args)
     ecl_stack_frame_close(name);
 
     auto b = clbind::wrap(cool2, name);
-    std::cout << clbind::toCpp<int>(b) << std::endl;
+    std::cout << "wrap" << clbind::toCpp<int>(b) << std::endl;
+
+    auto c = wrap2(cool2, name);
+    std::cout << "wrap2" << clbind::toCpp<int>(c) << std::endl;
 
     // Use stack
     // std::cout << clbind::toCpp<int>(clbind::nth_arg(name, 1)) << std::endl;
     // auto b = clbind::wrap(cool, name);
     // std::cout << clbind::toCpp<int>(b) << std::endl;
+
+    // ft(&ft_test);
+    // ft_member m;
+    // ft(ft_member::test);
+    //    ft(ft_op{});
 }
