503e7ff45d4696bbf753d4d69dbaa65214797502
Christoph <christoph.buck@smartoptics.de>
2019-08-14 16:58:55 +0200

    Add links for documentation

--- clbind/include/clbind/clbind.hpp
+++ clbind/include/clbind/clbind.hpp
@@ -4,6 +4,8 @@
 // https://github.com/Nelarius/wrenpp/blob/master/Wren%2B%2B.h
 // http://stackoverflow.com/questions/17339789/how-to-call-a-function-on-all-variadic-template-args
 // http://anthony.noided.media/blog/programming/c++/ruby/2016/05/12/mruby-cpp-and-template-magic.html
+// https://github.com/Islam0mar/CLCXX/blob/master/include/clcxx/package.hpp
+// https://github.com/juanjosegarciaripoll/cl-cxx/blob/master/include/cl-cxx/backend/ecl.hpp
 
 #include <clbind/ecl_utilities.hpp>
 #include <clbind/convert_types.hpp>
