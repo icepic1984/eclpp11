a637ee4a686d9cfdb9e44ea31d9fb97cd4893c72
Christoph Buck <christoph.buck@smartoptics.de>
2019-08-20 17:07:58 +0200

    Cleanup package

--- clbind/include/clbind/package.hpp
+++ clbind/include/clbind/package.hpp
@@ -21,78 +21,57 @@ public:
     explicit package(const std::string& name)
         : m_name(name){};
 
-    template <typename R, typename... Args>
-    void defun(const std::string& name, std::function<R(Args...)> f)
-    {
-        std::cout << "Name of fun: " << name << std::endl;
-    }
-
-    template <typename R, typename... Args>
-    void defun(const std::string& name, R (*f)(Args...))
-    {
-        defun(name, std::function<R(Args...)>(f));
-    }
-
-    template <typename LambdaT>
-    void defun(const std::string& name, LambdaT&& lambda)
-    {
-        add_lambda(name, std::forward<LambdaT>(lambda), &LambdaT::operator());
-    }
-
     template <typename F>
-    void defun2(const std::string& name, F&& func)
+    void defun(const std::string& name, F&& func)
     {
-        defun2Impl(name, std::forward<F>(func));
-    }
-
-    template <typename F>
-    auto defun3(const std::string& name, F&& func) -> decltype(auto)
-    {
-        // typename function_traits<F>::func_type f(std::forward<F>(func));
-        // typename function_traits<F>::func_type a;
-        // static_assert(std::is_same_v<typename function_traits<F>::func_type,
-        //     std::function<int(int)>>);
-        typename as_function<function_return_type_t<F>,
-            function_args_t<F>>::type f(std::forward<F>(func));
-        return f;
-        // return typename function_traits<F>::func_type f(std::forward(func));
+        defunImpl(name, std::forward<F>(func));
     }
 
 private:
     template <typename Return, typename... Args>
-    void defun2Impl(const std::string& name, Return (*func)(Args...))
+    void defunImpl(const std::string& name, Return (*func)(Args...))
     {
         std::function<Return(Args...)> function(
             std::forward<decltype(func)>(func));
-        std::cout << function(10, 20) << std::endl;
     }
 
-    template <typename Class, typename Return, typename... Args>
-    void defun2Impl(const std::string& name, Return (Class::*func)(Args...))
+    // For function member point
+    template <typename Return, typename Class, typename... Args>
+    void defunImpl(const std::string& name, Return (Class::*func)(Args...))
     {
-        defun2Impl<Return(Class&, Args...)>(
-            name, std::forward<decltype(func)>(func));
+        defunImpl<Return, Class, Args...>(name, func);
     }
 
-    template <typename R, typename Class, typename... Args>
-    void defun2Impl(
-        const std::string& name, Class&& functor, R (Class::*)(Args...) const)
+    // For const function member point
+    template <typename Return, typename Class, typename... Args>
+    void defunImpl(
+        const std::string& name, Return (Class::*func)(Args...) const)
     {
+        defunImpl<Return, const Class, Args...>(name, func);
     }
 
+    // For functor with const member function
+    template <typename Return, typename Class, typename... Args>
+    void defunImpl(const std::string& name, Class&& functor,
+        Return (Class::*func)(Args...) const)
+    {
+        defunImpl<Return, const Class, Args...>(
+            name, std::forward<Class>(functor), func);
+    }
+
+    // For functor with non constant member function
+    template <typename Return, typename Class, typename... Args>
+    void defunImpl(
+        const std::string& name, Class&& functor, Return (Class::*)(Args...))
+    {
+        std::function<Return(Args...)> f(std::forward<Class>(functor));
+    }
+
+    // For functor (dispatches to const vs non const overload)
     template <typename Functor>
-    void defun2Impl(const std::string& name, Functor&& functor)
+    void defunImpl(const std::string& name, Functor&& functor)
     {
-        defun2Impl(name, std::forward<Functor>(functor), &Functor::operator());
-    }
-
-    template <typename R, typename LambdaT, typename... ArgsT>
-    void add_lambda(const std::string& name, LambdaT&& lambda,
-        R (LambdaT::*)(ArgsT...) const)
-    {
-        std::cout << "lambda" << std::endl;
-        return defun(
-            name, std::function<R(ArgsT...)>(std::forward<LambdaT>(lambda)));
+        defunImpl(name, std::forward<Functor>(functor), &Functor::operator());
     }
 
     std::string m_name;
@@ -209,7 +188,7 @@ int bla(int a, int b)
     return a + b;
 }
 
-struct op
+struct functor
 {
     int fu = 100;
     int operator()(int a, int b)
@@ -218,59 +197,65 @@ struct op
     }
 };
 
-struct op2
+struct functor_const
+{
+    int fu = 100;
+    int operator()(int a, int b) const
+    {
+        return fu + a - b;
+    }
+};
+
+struct operator_test
 {
     int b = -100;
     int test(int a)
+    {
+        b = 10;
+        return a + 123 - b;
+    }
+};
+
+struct operator_test_const
+{
+    int b = -100;
+    int test(int a) const
     {
         return a + 123 - b;
     }
 };
 
-int a = 666;
+int a = 0;
+
 extern "C" {
-void reg()
-{
-
-    clbind::defun("BLA", "BLUP2", bla);
-    clbind::defun2("BLA", "BLUP3", [](int a, int b) { return a + b; });
-    clbind::defun2("BLA", "BLUP4", [&a](int b, int c) { return a + b + c; });
-    clbind::defun2("BLA", "BLUP5", op{});
-    // clbind::defun("BLA", "BLUP", [](int a, int b) { return a + b; });
-
-    // auto l = [](int a, int b) { return a + b; };
-    // func([&global](int a, int b) mutable { return a + b; });
-    //  clbind::defun("BLA", "BLUP3", [](int a, int b) { return a + b; });
-}
-
-void init()
-{
-    reg();
-}
 
 bool register_package(
     const char* name, void (*register_callback)(clbind::package&))
 {
     auto package = clbind::registry::get_registry().create_package(name);
     // register_callback(package);
-    package.defun2("bla", bla);
-    package.defun2("blup", [&a](int b) { return a + b; });
+    package.defun("blup", [](int a, int c) { return a; });
+    package.defun("blup", [&a](int b) { return a + b; });
+    package.defun("blup", [&a](int b) mutable { return a + b; });
 
-    auto b = package.defun3("blup", [](int a, int c) { return a; });
-    auto c = package.defun3("blup", [&a](int b) { return a + b; });
-    auto d = package.defun3("blup", op{});
-    auto e = package.defun3("blup", &op2::test);
-    std::cout << "defun3 b" << b(10, 20) << std::endl;
-    std::cout << "defun3 c" << c(100) << std::endl;
-    std::cout << "defun3 d" << d(10, 20) << std::endl;
-    op2 p;
-    std::cout << "defun3 e" << e(p, 10) << std::endl;
+    package.defun("blup", functor{});
+    package.defun("blup", functor_const{});
+    package.defun("blup", &operator_test::test);
+    package.defun("blup", &operator_test_const::test);
+    // package.defun("blup", [&a](int b) { return a + b; });
 
-    std::function<int(op2&&, int)> f(&op2::test);
-    std::cout << f(op2{}, 10) << std::endl;
-    std::cout << f(p, 10) << std::endl;
+    // package.defun("blup", &op2::test);
+    // std::cout << "defun3 b" << b(10, 20) << std::endl;
+    // std::cout << "defun3 c" << c(100) << std::endl;
+    // std::cout << "defun3 d" << d(10, 20) << std::endl;
+    // const op2 p;
+    // std::cout << "defun3 e" << e(p, 10) << std::endl;
+
+    // std::function<int(op2&, int)> f(&op2::test);
+
+    // std::cout << f(p, 10) << std::endl;
+    // std::cout << f(p, 10) << std::endl;
     return true;
 }
 }
-
 #define CLBIND_PACKAGE extern "C" void
